56c11f117df8db7831fa7490fd0ffe41b5b6d73f
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-05-12 12:02:22 +0530

    feat: implement controller methods

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function search()
+    {
+        $results = Customer::orderBy('firstname')
+            ->when(request('q'), function ($query) {
+                $query->where('firstname', 'like', '%' . request('q') . '%')
+                    ->orWhere('lastname', 'like', '%' . request('q') . '%')
+                    ->orWhere('email', 'like', '%' . request('q') . '%');
+            })
+            ->limit(6)
+            ->get();
+
+        return response()
+            ->json(['results' => $results]);
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function search()
+    {
+        $results = Product::orderBy('item_code')
+            ->when(request('q'), function ($query) {
+                $query->where('item_code', 'like', '%' . request('q') . '%')
+                    ->orWhere('description', 'like', '%' . request('q') . '%');
+            })
+            ->limit(6)
+            ->get();
+
+        return response()
+            ->json(['results' => $results]);
+    }
 }
