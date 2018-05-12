f64cf68e0884d5fd0cde3fc748d85a0c4a4b7e3f
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-05-12 19:10:58 +0530

    feat: implement Vue components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,16 @@
+import Vue from 'vue';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import App from './App.vue';
+import router from './router'
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.filter('formatMoney', (value) => {
+    return Number(value)
+        .toFixed(2)
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+})
 
 const app = new Vue({
-    el: '#app'
+    el: "#root",
+    render: h => h(App),
+    router
 });
